65724fc1dfdf460427b89bccb64cd27358c51ddd
13-arch <Iron_Mike@tutamail.com>
2025-05-31 12:10:58 +0300

    completed

--- var_10_3.py
+++ var_10_3.py
@@ -5,9 +5,9 @@ async def task(name, delay):
     print(f"Task {name} completed after {delay} seconds")
 
 async def main():
-    for i in range(0, 21,2):
+    for i in range(0, 22,2):
         list=['A', 0, 'B', 0,'C', 0,'D', 0,'E', 0,
-              'F', 0,'G', 0,'H', 0,'I', 0,'G',  0]
+              'F', 0,'G', 0,'H', 0,'I', 0,'G',0,'H']
         tasks = [
                 asyncio.create_task(task(list[i], i))
             ]
